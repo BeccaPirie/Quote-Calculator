e1d949e3376ff5798d35b5a60cd39d86803a920a
BeccaPirie <43371064+BeccaPirie@users.noreply.github.com>
2023-04-13 14:00:49 +0100

    config

---
+++ server/config.js
@@ -0,0 +1,11 @@
+const config = {
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 8000,
+    jwtSecret: process.env.JWT_SECRET || "secret_key_182",
+    mongoUri: process.env.MONGODB_URI ||
+              process.env.MONGO_HOST ||
+              // 'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGO_PORT || '27017')+'/quotes_database/' ||
+              'mongodb://127.0.0.1/quotes_database'
+  }
+  
+export default config
