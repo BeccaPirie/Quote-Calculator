f255b05a02f9d06eb8dd8d9b08c8587500b350ad
BeccaPirie <43371064+BeccaPirie@users.noreply.github.com>
2023-06-17 19:40:45 +0100

    remove scripts

--- database.sh
+++
@@ -1,5 +0,0 @@
-  1  cd rebecca
-  2  cd cm4025_assessment-BeccaPirie
-  3  mkdir database
-  4  cd database
-  5  mongod --dbpath .
--- server.sh
+++
@@ -1,8 +0,0 @@
-   1  mkdir rebecca
-   2  cd rebecca
-   3  git clone https://github.com/RobertGordonUniversity/cm4025_assessment-BeccaPirie.git
-   4  cd server
-   5  cd cm4025_assessment-BeccaPirie
-   6  cd server
-   7  npm install
-   8  node server.js
